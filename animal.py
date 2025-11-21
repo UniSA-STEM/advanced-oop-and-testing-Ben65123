b200a0629c7fdb7e537a001f2cf80be8f76ce853
benjaminsienicki <sieby003@mymail.unisa.edu.au>
2025-11-21 14:47:14 +1030

    Animal class created with validation to make sure each attribute does not come back empty. All attributes made private, getters, setters and propertys added for clean and safe way to access attributes.

--- animal.py
+++ animal.py
@@ -7,3 +7,72 @@ Username: sieby003
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
 
+# animal.py
+# Basic animal class for zoo system, stores the key attributes of an animal, allows for the creation of 1 animal at a time.
+# keeps the data private so that they cant be changed or tampered with from the outside. Have safe ways
+# to access and update the data with the getters, setters and property's.
+class Animal:
+    def __init__(self, name: str, species: str, age: int, diet: str, category: str):
+        # Validation to make sure values are not empty.
+        if not name:
+            raise ValueError("Animal name cannot be empty.")
+        if not species:
+            raise ValueError("Animal species cannot be empty.")
+        if age < 0:
+            raise ValueError("Animal age cannot be negative.")
+        if not diet:
+            raise ValueError("Animal diet cannot be empty.")
+        if not category:
+            raise ValueError("Animal category cannot be empty.")
+        # Private attributes created.
+        self.__name = name
+        self.__species = species
+        self.__age = age
+        self.__diet = diet
+        self.__category = category
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        if not name:
+            raise ValueError("Animal name cannot be empty.")
+        self.__name = name
+
+    def get_species(self):
+        return self.__species
+
+    def set_species(self, species):
+        if not species:
+            raise ValueError("Animal species cannot be empty.")
+        self.__species = species
+
+    def get_age(self):
+        return self.__age
+
+    def set_age(self, age):
+        if age < 0:
+            raise ValueError("Animal age cannot be negative.")
+        self.__age = age
+
+    def get_diet(self):
+        return self.__diet
+
+    def set_diet(self, diet):
+        if not diet:
+            raise ValueError("Animal diet cannot be empty.")
+        self.__diet = diet
+
+    def get_category(self):
+        return self.__category
+
+    def set_category(self, category):
+        if not category:
+            raise ValueError("Animal category cannot be empty.")
+        self.__category = category
+    # Properties for each attribute.
+    name = property(get_name, set_name)
+    species = property(get_species, set_species)
+    age = property(get_age, set_age)
+    diet = property(get_diet, set_diet)
+    category = property(get_category, set_category)
