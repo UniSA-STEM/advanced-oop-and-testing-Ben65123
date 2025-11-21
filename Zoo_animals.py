652d8c4fa3753bd5c1e80756b45f5b7f81ffd343
benjaminsienicki <sieby003@mymail.unisa.edu.au>
2025-11-21 15:32:26 +1030

    Added new file and added header.

---
+++ Zoo_animals.py
@@ -0,0 +1,8 @@
+'''
+File: Zoo_animals.py
+Description: A brief description of this Python module.
+Author: Benjamin Sienicki
+ID: 110442676
+Username: sieby003
+This is my own work as defined by the University's Academic Integrity Policy.
+'''
